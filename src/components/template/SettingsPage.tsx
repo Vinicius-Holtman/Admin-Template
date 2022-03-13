75d74497ca4de9cba79abdb5bd7e56aa2a81c491
vinicius <viniholtman28@gmail.com>
2022-03-13 16:22:34 -0300

    Created dark template and created context for content

--- src/components/template/SettingsPage.tsx
+++ src/components/template/SettingsPage.tsx
@@ -4,7 +4,7 @@ import { AwsSvg } from "../icons"
 
 export default function SettingsPage() {
     return (
-        <aside className='flex flex-col'>
+        <aside className='flex flex-col bg-gray-200 text-gray-700 dark:bg-gray-900'>
             <div className="flex flex-col items-center justify-center h-20 w-20 bg-gradient-to-r from-indigo-500 to-purple-800">
                 { AwsSvg }
             </div>
@@ -16,7 +16,7 @@ export default function SettingsPage() {
             <ul>
                 <MenuItem text="Logout" icon={LogoutIcon}
                 onLinkClick={() => console.log('logout')}
-                className={`text-red-600 hover:bg-red-400 hover:text-white`}/>
+                className={`text-red-600 dark:text-red-400 hover:bg-red-400 dark:hover:text-white hover:text-white`}/>
             </ul>
         </aside>
     )
--- src/pages/newsbells.tsx
+++ src/pages/newsbells.tsx
@@ -1,9 +1,14 @@
 import Layout from "../components/template/Layout";
+import useAppData from "../data/hook/useAppData";
 
 export default function NewsBells () {
+  const data = useAppData()
+
   return (
     <div>
-     <Layout title="Bells" subTitle="Here you will manage your notifications"/>
+     <Layout title="Bells" subTitle="Here you will manage your notifications">
+      <h3>{data.name}</h3>
+     </Layout>
     </div>
   )
 }
