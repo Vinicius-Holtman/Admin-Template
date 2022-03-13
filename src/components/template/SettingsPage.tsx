5c50cfd000f204674bb582d0dbab8b25e655ad33
vinicius <viniholtman28@gmail.com>
2022-03-12 21:34:01 -0300

    Created router link for navigation pages

--- src/components/template/SettingsPage.tsx
+++ src/components/template/SettingsPage.tsx
@@ -1,10 +1,14 @@
-
-
+import MenuItem from "./MenuItem";
+import { HomeIcon, AdjustmentsIcon, BellIcon } from '../icons/index'
 
 export default function SettingsPage() {
     return (
         <aside>
-            <h1>Menu</h1>
+            <ul>
+                <MenuItem url="/" text="Home" icon={HomeIcon}/>
+                <MenuItem url="/adjustments" text="Adjustments" icon={AdjustmentsIcon}/>
+                <MenuItem url="/newsbells" text="New Bell" icon={BellIcon}/>
+            </ul>
         </aside>
     )
 }
---
+++ src/pages/adjustments.tsx
@@ -0,0 +1,9 @@
+import Layout from "../components/template/Layout";
+
+export default function Adjustments() {
+  return (
+    <div>
+     <Layout title="Adjustments" subTitle="Customize here"/>
+    </div>
+  )
+}
